1f22b0945b5780da4ed0be7cd58750613610411a
tanyajha29 <jhatanya211@gmail.com>
2026-02-03 23:24:16 +0530

    local setup

---
+++ web-frontend/chemviz-web/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import './styles/app.css';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+}
